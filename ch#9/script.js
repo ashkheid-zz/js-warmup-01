df2cd498c92436769451873ad347de72eeb39c42
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2021-08-31 21:23:56 +0430

    BigFixed
    There was a typo in both functions.

--- ch#9/script.js
+++ ch#9/script.js
@@ -1,10 +1,10 @@
 function hijriLeapYear(year) {
-  return (year % 4 === 0);
-}
-
-function gregorianLeapYear(year) {
   return (year % 4 === 3);
 }
 
-console.log(hijriLeapYear(2021));
-console.log(gregorianLeapYear(1400));
+function gregorianLeapYear(year) {
+  return (year % 4 === 0);
+}
+
+console.log(hijriLeapYear(1400));
+console.log(gregorianLeapYear(2021));

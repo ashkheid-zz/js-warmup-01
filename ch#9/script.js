18208e57513bb350a6c6c4187c53f511ceddd127
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2021-09-02 20:19:39 +0430

    more precise solution implemented

--- ch#9/script.js
+++ ch#9/script.js
@@ -1,9 +1,25 @@
 function hijriLeapYear(year) {
-  return (year % 4 === 3);
+  let num = 0.24219858156;
+  let a = (year + 2346) * num;
+  return a - parseInt(a) < num;
 }
 
 function gregorianLeapYear(year) {
-  return (year % 4 === 0);
+  /*
+  if (year is not divisible by 4) then (it is a common year)
+  else if (year is not divisible by 100) then (it is a leap year)
+  else if (year is not divisible by 400) then (it is a common year)
+  else (it is a leap year)
+  */
+  if (year % 4 !== 0) {
+    return false;
+  } else if (year % 100 !== 0) {
+    return true;
+  } else if (year % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 console.log(hijriLeapYear(1400));

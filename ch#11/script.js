8eb25a87baf5cf4df839d6403c1760fd809dcd51
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2021-09-02 19:49:12 +0430

    Solved

--- ch#11/script.js
+++ ch#11/script.js
@@ -1,9 +1,25 @@
-/*----- Solution 1 ----*/
+let messageBag = {
+  exceptions: {
+    1: {
+      num: 1009,
+      message: "Network Connection Error!",
+    },
+    2: {
+      num: 1002,
+      message: "User is not logged-in.",
+    },
+  },
+};
 
+function prettyPrintMessageLog(jsObj) {
+  let now = new Date();
+  for (let item in jsObj.exceptions) {
+    console.log(
+      `${now.getFullYear()}-${now.getMonth()}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} Error Number ${
+        jsObj.exceptions[item].num
+      } : ${jsObj.exceptions[item].message}`
+    );
+  }
+}
 
-
-/*----- Solution 2 ----*/
-
-
-
-/*----- Solution 3 ----*/
+prettyPrintMessageLog(messageBag);

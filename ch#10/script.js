e6ef9468e407788a6d52b45e303dbcb282802233
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2021-09-07 17:13:55 +0430

    Code simplified

--- ch#10/script.js
+++ ch#10/script.js
@@ -1,43 +1,21 @@
-let product_count;
+let produc_count = 5;
 
-function isNumber(num) {
-  if (isNaN(num) || typeof(num) != "number") {
-    throw "The argument you passed is not a number! please pass a number.";
+function increase(num = 1) {
+  if (num > 1 && num >= produc_count) {
+    produc_count = num;
+    return produc_count;
   }
-  return true;
+  return ++produc_count;
 }
 
-function increase() {
-  for (let i = 0; i < arguments.length; ++i) {
-    isNumber(parseInt(arguments[i]));
+function decrease(num = 1) {
+  if (produc_count === 0 || produc_count < num) {
+    return console.log("ERROR!");
+  } else if (num > 1 && num <= produc_count) {
+    produc_count = num;
+    return produc_count;
   }
-
-  if (arguments.length === 0) {
-    increase1();
-  }
-
-  if (arguments.length === 1) {
-    increase2(arguments[0]);
-  }
+  return --produc_count;
 }
 
-function increase1() {
-  if (product_count === undefined) {
-    product_count = 0;
-  }
-
-  ++product_count;
-}
-
-function increase2(num) {
-  product_count = (num < product_count) ? product_count : num;
-}
-
-
-console.log(product_count);
-increase();
-console.log(product_count);
-increase(12);
-console.log(product_count);
-increase(5);
-console.log(product_count);
+console.log(produc_count);
